cb4560535adbd5b94f319d2f3e671a34394ca9c3
alvin <liulimin@deepexi.com>
2019-03-17 23:37:56 +0800

    添加布局容器组件

---
+++ no-page/src/components/layout-container.vue
@@ -0,0 +1,17 @@
+<script>
+  import CompContainer from '@/components/comp-container'
+  export default {
+    name: 'layout-container',
+    props: {
+      id: [String, Number]
+    },
+    render (h) {
+      return h(CompContainer, {
+        props: {
+          containerId: this.id
+        }
+      })
+    }
+  }
+</script>
+
